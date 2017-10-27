7f85a6c56afad2c57c2a7c9686a79853a220730d
Musa Raza <raza.musa@gmail.com>
2017-10-27 16:53:12 -0400

    Complete DFS

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,6 +1,11 @@
 module Searchable
   def dfs(target_value)
-
+    return self if self.value == target_value
+    self.children.each do |node|
+      result = node.dfs(target_value)
+      return result unless result == nil
+    end
+    nil
   end
 
   def bfs(target_value)
@@ -9,24 +14,33 @@ module Searchable
 end
 
 class PolyTreeNode
-
+  include Searchable
   attr_accessor :value
 
   attr_reader :parent, :children
 
   def initialize(value)
-
+    @value = value
+    @parent = nil
+    @children = []
   end
 
   def parent=(node)
-
+    return if self.parent == node
+    if self.parent
+      self.parent.children.delete(self)
+    end
+    @parent = node
+    self.parent.children << self unless self.parent.nil?
   end
 
   def add_child(child_node)
+    child_node.parent = self
   end
 
   def remove_child(child_node)
-    
+    raise "Not a child" unless @children.include?(child_node)
+    child_node.parent = nil
   end
 
 end
